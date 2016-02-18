3847f3c30fdb10b3a4bbdb7b644ab0798976adbc
Sixten83 <sixten.isaksson@terco.se>
2016-02-18 12:59:55 +0100

    done

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.CustomLabel customLabel1 = new System.Windows.Forms.DataVisualization.Charting.CustomLabel();
+            System.Windows.Forms.DataVisualization.Charting.CustomLabel customLabel2 = new System.Windows.Forms.DataVisualization.Charting.CustomLabel();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.CustomLabels.Add(customLabel1);
+            chartArea1.AxisY.CustomLabels.Add(customLabel2);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(32, 84);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series2.Legend = "Legend1";
+            series2.Name = "MouseZoom";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(562, 485);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(307, 27);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(65, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "FastLine";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(447, 27);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(54, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Spline";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(1245, 138);
+            this.trackBar1.Maximum = 100000;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(193, 45);
+            this.trackBar1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1328, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 33);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1450, 606);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        int samples = 45000;
+        int amplitude = 20;
+        GraphPane graph;
+        Double[] x,y;
+        bool changing = false;
+        double startX, startY,endX,endY;
+        double panStartX, panStartY, panEndX, panEndY;
+        double panDeltaX, panDeltaY;
+        bool mouseLeftpressed, mouseWheelPressed;
+        Point mouseDown;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.timer1.Interval = 10;
+            this.timer1.Start();
+            this.trackBar1.ValueChanged += new System.EventHandler(trackBar1_ValueChanged);
+            chart1.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            chart1.ChartAreas[0].AxisX.Maximum = samples;
+            chart1.ChartAreas[0].AxisX.Interval = samples / 10;
+
+            chart1.ChartAreas[0].AxisY.Interval = 4;
+            chart1.ChartAreas[0].AxisY.Minimum = -20;
+            chart1.ChartAreas[0].AxisY.Maximum = 20;
+            x = new double[samples];
+            y = new double[samples];
+            chart1.Series["Series1"].Points.DataBindXY(x, y);
+            chart1.MouseWheel += new System.Windows.Forms.MouseEventHandler(zoom_MouseWheel);
+            chart1.MouseMove += new System.Windows.Forms.MouseEventHandler(zoom_MouseMove);
+            chart1.MouseUp += new System.Windows.Forms.MouseEventHandler(zoom_MouseUp);
+            chart1.MouseDown += new System.Windows.Forms.MouseEventHandler(zoom_MouseDown);
+            
+        }
+
+        private void zoom_MouseDown(object sender, EventArgs e)
+        {
+            chart1.Focus();
+        }
+        private void zoom_MouseMove(object sender, EventArgs e)
+        {
+            MouseEventArgs mouse = e as MouseEventArgs;
+
+            if (mouse.Button == MouseButtons.Left)
+            {
+                Point mousePosNow = mouse.Location;
+
+                if (!mouseLeftpressed)
+                {
+                    mouseLeftpressed = true;
+                    mouseDown = mouse.Location;
+                    try
+                    {
+                        startX = chart1.ChartAreas[0].AxisX.PixelPositionToValue(mouseDown.X);
+                        startY = chart1.ChartAreas[0].AxisY.PixelPositionToValue(mouseDown.Y);
+                    }
+                    catch (ArgumentException)
+                    {
+
+                    }
+                    catch (InvalidOperationException)
+                    {
+
+                    }
+                }
+                // the distance the mouse has been moved since mouse was pressed
+                int deltaX = mousePosNow.X - mouseDown.X;
+                int deltaY = mousePosNow.Y - mouseDown.Y;
+
+                try
+                {
+                    endX = chart1.ChartAreas[0].AxisX.PixelPositionToValue(mousePosNow.X);
+                    endY = chart1.ChartAreas[0].AxisY.PixelPositionToValue(mousePosNow.Y);
+                }
+                catch (ArgumentException)
+                {
+
+                }
+                catch (InvalidOperationException)
+                {
+
+                }
+                // calculate new offset of image based on the current zoom factor
+               
+                chart1.Series["MouseZoom"].Points.Clear();
+                chart1.Series["MouseZoom"].Points.AddXY(startX, startY);
+                chart1.Series["MouseZoom"].Points.AddXY(endX, startY);
+                chart1.Series["MouseZoom"].Points.AddXY(endX, endY);
+                chart1.Series["MouseZoom"].Points.AddXY(startX, endY);
+                chart1.Series["MouseZoom"].Points.AddXY(startX, startY);
+            }
+            else if (mouse.Button == MouseButtons.Middle)
+            {
+                Point mousePosNow = mouse.Location;
+
+                if (!mouseWheelPressed)
+                {
+                    mouseWheelPressed = true;
+                    mouseDown = mouse.Location;
+
+                    panStartX = chart1.ChartAreas[0].AxisX.PixelPositionToValue(mouseDown.X);
+                    panStartY = chart1.ChartAreas[0].AxisY.PixelPositionToValue(mouseDown.Y);
+                    panDeltaX = chart1.ChartAreas[0].AxisX.Maximum - chart1.ChartAreas[0].AxisX.Minimum;
+                    panDeltaY = chart1.ChartAreas[0].AxisY.Maximum - chart1.ChartAreas[0].AxisY.Minimum;
+                }
+
+                try
+                {
+                    panEndX = chart1.ChartAreas[0].AxisX.PixelPositionToValue(mousePosNow.X);
+                    panEndY = chart1.ChartAreas[0].AxisY.PixelPositionToValue(mousePosNow.Y);
+                }
+                catch (ArgumentException)
+                {
+
+                }
+                catch (InvalidOperationException)
+                {
+
+                }
+
+                    double max, min;
+                    max = chart1.ChartAreas[0].AxisX.Maximum - (panEndX - panStartX);
+                    max = Math.Min(max, samples);
+                    min = chart1.ChartAreas[0].AxisX.Minimum - (panEndX - panStartX);
+                    min = Math.Max(min, 0);              
+
+                if (max - min == panDeltaX)
+                { 
+                    chart1.ChartAreas[0].AxisX.Maximum = max;
+                    chart1.ChartAreas[0].AxisX.Minimum = min;
+                }
+
+                    max = chart1.ChartAreas[0].AxisY.Maximum - (panEndY - panStartY);
+                    max = Math.Min(max, amplitude);
+                    min = chart1.ChartAreas[0].AxisY.Minimum - (panEndY - panStartY);
+                    min = Math.Max(min, -amplitude);
+
+                if (max - min == panDeltaY)
+                {
+                    chart1.ChartAreas[0].AxisY.Maximum = max;
+                    chart1.ChartAreas[0].AxisY.Minimum = min;
+                }
+                
+                
+
+               
+
+            }
+             
+        }
+
+        private void zoom_MouseUp(object sender, EventArgs e)
+        {
+            
+            mouseWheelPressed = false;
+
+            if (mouseLeftpressed && Math.Abs(endX - startX) >= 1 && Math.Abs(endY - startY) >= 0.1)
+            {
+                
+                chart1.ChartAreas[0].AxisX.Maximum = Math.Max(startX, endX);
+                chart1.ChartAreas[0].AxisX.Maximum = Math.Min(chart1.ChartAreas[0].AxisX.Maximum, samples);
+                chart1.ChartAreas[0].AxisX.Minimum = Math.Min(startX, endX);
+                chart1.ChartAreas[0].AxisX.Minimum = Math.Max(chart1.ChartAreas[0].AxisX.Minimum, 0);
+                chart1.ChartAreas[0].AxisX.Interval = Math.Abs(chart1.ChartAreas[0].AxisX.Maximum - chart1.ChartAreas[0].AxisX.Minimum) / 10;
+            
+                chart1.ChartAreas[0].AxisY.Maximum = Math.Max(startY, endY);
+                chart1.ChartAreas[0].AxisY.Maximum = Math.Min(chart1.ChartAreas[0].AxisY.Maximum, amplitude);
+                chart1.ChartAreas[0].AxisY.Minimum = Math.Min(startY, endY);
+                chart1.ChartAreas[0].AxisY.Minimum = Math.Max(chart1.ChartAreas[0].AxisY.Minimum, -amplitude);
+                chart1.ChartAreas[0].AxisY.Interval = Math.Abs(endY - startY) / 10;
+
+            }
+            mouseLeftpressed = false;
+            chart1.Series["MouseZoom"].Points.Clear();
+
+            
+        }
+
+      
+        private void zoom_MouseWheel(object sender, MouseEventArgs e)
+        {
+
+            
+            double delta, min, max, middle;
+
+            middle = chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.X);
+
+            double zoomFactor = (1 - 0.25 * e.Delta / 120);
+            
+            max = chart1.ChartAreas[0].AxisX.Maximum * zoomFactor;
+            min = chart1.ChartAreas[0].AxisX.Minimum * zoomFactor;            
+            delta = max - min;
+
+            if (middle + delta / 2 > samples)
+            {
+                max = samples;
+                min = samples - delta;
+
+            }
+            else if (middle - delta / 2 < 0) 
+            {
+                min = 0;
+                max = delta;
+            }
+            else
+            {
+                max = middle + delta / 2;
+                min = middle - delta / 2;
+            }
+
+            if(max - min < samples)
+            {
+                chart1.ChartAreas[0].AxisX.Maximum = max;
+                chart1.ChartAreas[0].AxisX.Minimum = min;     
+            }
+            else
+            {
+                chart1.ChartAreas[0].AxisX.Maximum = samples;
+                chart1.ChartAreas[0].AxisX.Minimum = 0;    
+            }
+
+            middle = chart1.ChartAreas[0].AxisY.PixelPositionToValue(e.Y);
+            max = chart1.ChartAreas[0].AxisY.Maximum * zoomFactor;
+            min = chart1.ChartAreas[0].AxisY.Minimum * zoomFactor;
+            delta = max - min;
+
+            if (middle + delta / 2 > amplitude)
+            {
+                max = amplitude;
+                min = amplitude - delta;
+
+            }
+            else if (middle - delta / 2 < -amplitude)
+            {
+                min = -amplitude;
+                max = -amplitude + delta;
+            }
+            else
+            {
+                max = middle + delta / 2;
+                min = middle - delta / 2;
+
+            }
+
+            if (max - min < 2 * amplitude)
+            {
+                chart1.ChartAreas[0].AxisY.Maximum = max;
+                chart1.ChartAreas[0].AxisY.Minimum = min;
+            }
+            else
+            {
+                chart1.ChartAreas[0].AxisY.Maximum = amplitude;
+                chart1.ChartAreas[0].AxisY.Minimum = -amplitude;
+            }
+
+            chart1.ChartAreas[0].AxisX.Interval = (chart1.ChartAreas[0].AxisX.Maximum - chart1.ChartAreas[0].AxisX.Minimum) / 10;
+            chart1.ChartAreas[0].AxisY.Interval = (chart1.ChartAreas[0].AxisY.Maximum - chart1.ChartAreas[0].AxisY.Minimum) / 10;
+            /*
+            //chart1.ChartAreas[0].AxisY.Maximum = amplitude;
+            chart1.ChartAreas[0].AxisY.Minimum = -amplitude;
+            chart1.ChartAreas[0].AxisY.Interval = amplitude * 2 / 10;
+            */
+           
+        }
+
+        public void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            changing = true;
+            label1.Text = trackBar1.Value.ToString();
+            samples = trackBar1.Value;
+            x = new double[samples];
+            y = new double[samples];
+            
+            changing = false;
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(!changing)
+            { 
+                chart1.Series.SuspendUpdates();
+                Random rand = new Random();
+                chart1.Series["Series1"].Points.Clear();
+                for (int r = 0; r < samples || changing; r += 1)
+                {
+                    //chart1.Series["Series1"].Points.AddXY(r, 15 * Math.Sin(r / 100) + rand.NextDouble());
+                   x[r] = r;
+                   y[r] = 15 * Math.Sin((double)r / 10000) + rand.NextDouble();
+                }
+                updateChart(x, y,samples);
+   
+                chart1.Series.ResumeUpdates();               
+            }
+        }
+
+        public void updateChart(Double[] x, Double[] y, int samples)
+        {
+            int max = (int)chart1.ChartAreas[0].AxisX.Maximum;
+            int min = (int)chart1.ChartAreas[0].AxisX.Minimum;
+
+            Double downsample;
+            if ((max - min) > 2000)
+                downsample = (double)(max - min) / 2000;
+            else
+                downsample = 1;
+
+            int _samples = 0;
+            int r = min;
+            for (; r < max; r += (int)downsample)
+            {
+                chart1.Series["Series1"].Points.AddXY(x[r], y[r]);
+               // if (_samples == 2000)
+                 //   break;
+                _samples++;
+            }
+         //   Console.WriteLine(x[r - 1]);
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(radioButton1.Checked)
+                chart1.Series["Series1"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            chart1.Series["Series1"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+        }
+
+       
+    }
+}
